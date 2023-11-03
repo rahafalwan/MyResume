62732c1c686f1fffde08479e4ebf30e67d471741
rahafalwan <rahaf.alwan.prog@gmail.com>
2023-11-03 16:18:09 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+       
+       
+        function rating() {
+            document.getElementById('bar-50').style.width='50%';
+            document.getElementById('bar-65').style.width='65%';
+            document.getElementById('bar-80').style.width='80%';
+            document.getElementById('bar-81').style.width='80%';
+            document.getElementById('bar-45').style.width='45%';
+          
+          }
+          openPage()
+          {
+            
+          }
